ae025a770b3677f735c03db42ba2576892832594
Jan <jan@seifert-online.de>
2023-08-27 22:35:54 +0200

    revert back to keyboard

--- gamestates.py
+++ gamestates.py
@@ -10,27 +10,22 @@ def menu():
     screenX, screenY = 0, 0
     logo = headline("Snake")
     update = False
-    with menulistener:
-        while True:
-            if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY or update:
-                if not update:
-                    hardClear()
-                update = False
-                screenX, screenY = get_terminal_size()
-                centeredLogo = centerMultiline(logo, screenX)
-                print(CMT + centeredLogo)
-                print("Press Space to start Game".center(screenX))
-                print("Press x to access Settings".center(screenX))
-            if keyspecial("space") in menukeys:
-                while keyspecial("space") in menukeys:
-                    pass
-                return
-            if keynormal("x") in menukeys:
-                while keynormal("x") in menukeys:
-                    pass
-                settings()
-                print(CLR)
-                update = True
+    while True:
+        if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY or update:
+            if not update:
+                hardClear()
+            update = False
+            screenX, screenY = get_terminal_size()
+            centeredLogo = centerMultiline(logo, screenX)
+            print(CMT + centeredLogo)
+            print("Press Space to start Game".center(screenX))
+            print("Press x to access Settings".center(screenX))
+        if keyboard.is_pressed("space"):
+            return
+        if keyboard.is_pressed("x"):
+            settings()
+            print(CLR)
+            update = True
 
 def settings():
     with open("settings.json", "r") as f:
@@ -40,44 +35,40 @@ def settings():
     selection = 0
     print(CLR)
     update = False
-    with settingslistener:
-        while True:
-            if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY or update:
-                update = False
-                screenX, screenY = get_terminal_size()
-                print(CMT + "Settings".center(screenX))
-                print("To navigate use arrow keys ↑↓, to select press enter <╝. Press c to cancel".center(screenX))
-                print()
-                print(f"Render mode: <{colorDict[selection==0]}{SETTINGS_rendermode[0][SETTINGS_rendermode[1]]}{COLORRESET}>".center(screenX))
-                print(f"Save Highscore: <{colorDict[selection==1]}{SETTINGS_savehs[0][SETTINGS_savehs[1]]}{COLORRESET}>".center(screenX))
-                print(f"increasing speed: <{colorDict[selection==2]}{SETTINGS_speedadj[0][SETTINGS_speedadj[1]]}{COLORRESET}>".center(screenX))
-            if keyspecial("arrowup") in settingskeys:
-                while keyspecial("arrowup") in settingskeys:
-                    pass
-                selection = max(0, selection-1)
-                update = True
-            if keyspecial("arrowdown") in settingskeys:
-                while keyspecial("arrowdown") in settingskeys:
-                    pass
-                selection = min(2, selection+1)
-                update = True
-            if keyspecial("enter") in settingskeys:
-                while keyspecial("enter") in settingskeys:
-                    pass
-                match selection:
-                    case 0:
-                        SETTINGS_rendermode = [SETTINGS_rendermode[0], 1-SETTINGS_rendermode[1]]
-                    case 1:
-                        SETTINGS_savehs = [SETTINGS_savehs[0], 1-SETTINGS_savehs[1]]
-                    case 2:
-                        SETTINGS_speedadj = [SETTINGS_savehs[0], 1-SETTINGS_speedadj[1]]
-                update = True
-            if keynormal("c") in settingskeys:
-                while keynormal("c") in settingskeys:
-                    pass
-                with open("settings.json", "w") as f:
-                    f.write(json.dumps([SETTINGS_rendermode, SETTINGS_savehs, SETTINGS_speedadj], indent=4))
-                return
+    while True:
+        if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY or update:
+            update = False
+            screenX, screenY = get_terminal_size()
+            print(CMT + "Settings".center(screenX))
+            print("To navigate use arrow keys ↑↓, to select press enter <╝. Press c to cancel".center(screenX))
+            print()
+            print(f"Render mode: <{colorDict[selection==0]}{SETTINGS_rendermode[0][SETTINGS_rendermode[1]]}{COLORRESET}>".center(screenX))
+            print(f"Save Highscore: <{colorDict[selection==1]}{SETTINGS_savehs[0][SETTINGS_savehs[1]]}{COLORRESET}>".center(screenX))
+            print(f"increasing speed: <{colorDict[selection==2]}{SETTINGS_speedadj[0][SETTINGS_speedadj[1]]}{COLORRESET}>".center(screenX))
+        if keyboard.is_pressed("up arrow"):
+            selection = max(0, selection-1)
+            while keyboard.is_pressed("up arrow"):
+                pass
+            update = True
+        if keyboard.is_pressed("down arrow"):
+            selection = min(2, selection+1)
+            while keyboard.is_pressed("down arrow"):
+                pass
+            update = True
+        if keyboard.is_pressed("enter"):
+            match selection:
+                case 0:
+                    SETTINGS_rendermode = [SETTINGS_rendermode[0], 1-SETTINGS_rendermode[1]]
+                case 1:
+                    SETTINGS_savehs = [SETTINGS_savehs[0], 1-SETTINGS_savehs[1]]
+                case 2:
+                    SETTINGS_speedadj = [SETTINGS_savehs[0], 1-SETTINGS_speedadj[1]]
+            while keyboard.is_pressed("enter"):
+                pass
+            update = True
+        if keyboard.is_pressed("c"):
+            numpy.save("settings", [SETTINGS_rendermode, SETTINGS_savehs, SETTINGS_speedadj])
+            return
 
 def start():
     with open("settings.json", "r") as f:
@@ -87,36 +78,24 @@ def start():
     screenX, screenY = 0, 0
     gameController = Game(20, 10*{True: 2, False: 1}[detailedrender])
     gameController.spawnTreat()
-    def gameonpress(key):
-        if key == keynormal("w"):
-            gameController.changeDir("w")
-        if key == keyspecial("arrowup"):
-            gameController.changeDir("w")
-        if key == keynormal("a"):
-            gameController.changeDir("a")
-        if key == keyspecial("arrowleft"):
-            gameController.changeDir("a")
-        if key == keynormal("s"):
-            gameController.changeDir("s")
-        if key == keyspecial("arrowdown"):
-            gameController.changeDir("s")
-        if key == keynormal("d"):
-            gameController.changeDir("d")
-        if key == keyspecial("arrowright"):
-            gameController.changeDir("d")
-    def gameonrelease(key):
-        pass
-    gamelistener = pynput.keyboard.Listener(on_press=gameonpress, on_release=gameonrelease)
-    with gamelistener:
-        while True:
-            if gameController.move() == -1:
-                return gameController.score
-            if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY:
-                screenX, screenY = get_terminal_size()
-                hardClear()
-            print(CMT)
-            gameController.displayGame()
-            sleep(1/gameController.speed)
+    keyboard.add_hotkey("w", gameController.changeDir, args=("w"))
+    keyboard.add_hotkey("up arrow", gameController.changeDir, args=("w"))
+    keyboard.add_hotkey("a", gameController.changeDir, args=("a"))
+    keyboard.add_hotkey("left arrow", gameController.changeDir, args=("a"))
+    keyboard.add_hotkey("s", gameController.changeDir, args=("s"))
+    keyboard.add_hotkey("down arrow", gameController.changeDir, args=("s"))
+    keyboard.add_hotkey("d", gameController.changeDir, args=("d"))
+    keyboard.add_hotkey("right arrow", gameController.changeDir, args=("d"))
+    while True:
+        if gameController.move() == -1:
+            keyboard.remove_all_hotkeys()
+            return gameController.score
+        if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY:
+            screenX, screenY = get_terminal_size()
+            hardClear()
+        print(CMT)
+        gameController.displayGame()
+        sleep(1/gameController.speed)
 
 def gameover(score):
     with open("settings.json", "r") as f:
@@ -132,33 +111,26 @@ def gameover(score):
                 with open("data.json", "w") as f:
                     f.write(json.dumps([score]))
         else:
-            with open("data.json", "w") as f:
-                f.write(json.dumps([score]))
-    with gameoverlistener:
-        while True:
-            if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY:
-                screenX, screenY = get_terminal_size()
-                centeredLogo = centerMultiline(logo, screenX)
-                highscore = score
-                if os.path.exists("data.json"):
-                    with open("data.json", "r") as f:
-                        read = json.load(f)[0]
-                    if score > read:
-                        with open("data.json", "w") as f:
-                            f.write(json.dumps([score]))
-                    else:
-                        highscore = read
+            numpy.save("data", [score])
+    while True:
+        if get_terminal_size().columns != screenX or get_terminal_size().lines != screenY:
+            screenX, screenY = get_terminal_size()
+            centeredLogo = centerMultiline(logo, screenX)
+            highscore = score
+            if os.path.exists("data.npy"):
+                read = numpy.load("data.npy")[0]
+                if score > read:
+                    numpy.save("data.npy", [score])
                 else:
-                    with open("data.json", "w") as f:
-                        f.write(json.dumps([score]))
-                print(CMT + centeredLogo)
-                print(f"Score: {score}          Highscore: {highscore}".center(screenX))
-                print()
-                print("Press q to exit".center(screenX))
-                print("Press r to restart".center(screenX))
-            if keynormal("q") in gameoverkeys:
-                exit()
-            if keynormal("r") in gameoverkeys:
-                while keynormal("r") in gameoverkeys:
-                    pass
-                return
+                    highscore = read
+            else:
+                numpy.save("data.npy", [score])
+            print(CMT + centeredLogo)
+            print(f"Score: {score}          Highscore: {highscore}".center(screenX))
+            print()
+            print("Press q to exit".center(screenX))
+            print("Press r to restart".center(screenX))
+        if keyboard.is_pressed("q"):
+            exit()
+        if keyboard.is_pressed("r"):
+            return
